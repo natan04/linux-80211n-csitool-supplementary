5008813f5d45ab5dea0d276556fad9d6f56fda82
Natan <natan04@gmail.com>
2017-03-07 17:05:11 -0500

    add computer script

---
+++ scripts/listen_and_send.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import socket
+import sys
+
+
+
+UDP_IP = "132.72.80.189"
+UDP_PORT = 5005
+
+MESSAGE = "Hello, World!"
+
+
+
+def main(argv):
+	print "UDP target IP:", UDP_IP
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
