5daf6f245652932449406a43a9607e987cca3084
natan <natan04@gmail.com>
2017-03-09 07:55:35 -0800

    socket udp

---
+++ scripts/OpenWrt-SDK-15.05.1-ar71xx-generic_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64/package/helloworld/src/helloworld.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+ #include <sys/types.h>
+
+#define BUFLEN 512  //Max length of buffer
+#define PORT 5005   //The port on which to listen for incoming data
+int main(void)
+{
+
+
+	struct sockaddr_in si_me, si_other;
+     
+    int s, i, slen = sizeof(si_other) , recv_len;
+    char buf[BUFLEN];
+     
+    //create a UDP socket
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+          printf("problem : socket()");
+            return -1;
+    }
+    
+    printf("socket number: %d", s);
+    // zero out the structure
+    memset((char *) &si_me, 0, sizeof(si_me));
+     
+    si_me.sin_family = AF_INET;
+    si_me.sin_port = htons(PORT);
+    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+     
+    //bind socket to port
+    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
+    {
+
+  			printf("problem : bind()");
+            return -1;  
+     }
+     
+    //keep listening for data
+    while(1)
+    {
+        printf("Waiting for data...");
+        fflush(stdout);
+         
+        //try to receive some data, this is a blocking call
+        if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
+        {
+            printf("problem : recvfrom()");
+            return -1;
+        }
+         
+        //print details of the client/peer and the data received
+        printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+        printf("Data: %s\n" , buf);
+         
+        //now reply the client with the same data
+        if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)
+        {
+            printf("problem : sendto()");
+            return -1;
+        }
+    }
+ 
+    close(s);
+
+	return 0;
+}
+
