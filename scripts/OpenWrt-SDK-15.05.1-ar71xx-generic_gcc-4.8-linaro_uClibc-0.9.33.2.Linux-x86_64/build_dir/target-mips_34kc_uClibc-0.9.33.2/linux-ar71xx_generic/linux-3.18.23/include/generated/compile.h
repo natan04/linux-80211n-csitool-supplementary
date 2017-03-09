01710f6f3b73d1a2278fec5e3b01200de697b44c
natan <natan04@gmail.com>
2017-03-09 06:33:52 -0800

    add sdk

---
+++ scripts/OpenWrt-SDK-15.05.1-ar71xx-generic_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64/build_dir/target-mips_34kc_uClibc-0.9.33.2/linux-ar71xx_generic/linux-3.18.23/include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 1 */
+#define UTS_MACHINE "mips"
+#define UTS_VERSION "#1 Sun Jan 31 18:39:35 CET 2016"
+#define LINUX_COMPILE_BY "buildbot"
+#define LINUX_COMPILE_HOST "builder1"
+#define LINUX_COMPILER "gcc version 4.8.3 (OpenWrt/Linaro GCC 4.8-2014.04 r47269) "
